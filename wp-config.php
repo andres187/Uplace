09015931be1fad5edb07e24f5009a9565ac85bb2
andres187 <andresfebo187@gmail.com>
2015-05-19 14:20:46 -0500

    COrregido

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'w');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '/');
 
 /** MySQL database password */
-define('DB_PASSWORD', '895682644');
+define('DB_PASSWORD', '*');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
